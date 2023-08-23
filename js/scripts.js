a27b781120c8e4803cdcad0da3ecae0dab21b667
TomoriComAncora <lucas_fernando@ufms.br>
2023-08-23 09:59:12 -0400

    fim_js

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,31 @@ const multiplicacaoForm = document.querySelector("#multiplicacao-form");
 const numeroInput = document.querySelector("#number");
 const multiplicadorInput = document.querySelector("#multiplicador");
 
+const multiplicacaoTable = document.querySelector("#multiplicacao-operacao");
+const multiplicacaoTitulo = document.querySelector("#multiplicacao-title span");
+
 // fuções
+const tabelaMultiplicacao = (numero, numeroMultiplicado) => {
+  multiplicacaoTable.innerHTML = "";
+
+  for (let i = 1; i <= numeroMultiplicado; i++) {
+    let result = numero * i;
+    console.log(result);
+
+    const template = `<div class="row">
+        <div class="operacao">${numero} x ${i} =</div>
+        <div class="resultado">${result}</div>
+      </div>`;
+
+    const parser = new DOMParser()
+
+    const htmlTemplate = parser.parseFromString(template, "text/html");
+
+    const row = htmlTemplate.querySelector(".row");
+    multiplicacaoTable.appendChild(row);
+    multiplicacaoTitulo.innerText = numero;
+  }
+};
 
 // eventos
 multiplicacaoForm.addEventListener("submit", (e) => {
@@ -11,7 +35,7 @@ multiplicacaoForm.addEventListener("submit", (e) => {
   const numeroMultiplicado = +numeroInput.value;
   const numeroMultiplicacao = +multiplicadorInput.value;
 
-  if(!numeroMultiplicacao || !numeroMultiplicado) return;
-  
-  console.log(numeroMultiplicado, numeroMultiplicacao);
+  if (!numeroMultiplicacao || !numeroMultiplicado) return;
+
+  tabelaMultiplicacao(numeroMultiplicado, numeroMultiplicacao);
 });
